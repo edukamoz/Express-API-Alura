56520bef33fd1b5769709f9b54aa87d9634f55df
Eduardo Kamo <eduardokamoz@gmail.com>
2025-03-04 17:00:39 -0300

    Banco de dados
    Criando a conexão com o banco de dados MongoDB Atlas através de outra biblioteca de interface chamada Mongoose

--- src/app.js
+++ src/app.js
@@ -1,6 +1,19 @@
+// Importando a biblioteca do framework Express
 import express from "express"
+import conectaNaDataBase from "./config/dbConnect.js"
+
+const conexao = await conectaNaDataBase()
+
+conexao.on("error", (error) => {
+    console.error("Erro de conexão", erro)
+})
+
+conexao.once("open", () => {
+    console.log("Conexão com o banco feita com sucesso")
+})
 
 const app = express()
+// Permitir a "tradução" de String para json e vice-versa
 app.use(express.json())
 
 const livros = [
@@ -14,11 +27,14 @@ const livros = [
     }
 ]
 
+// ---------------- Funções ----------------
 function buscaLivro(id) {
     return livros.findIndex(livro => {
         return livro.id === Number(id)
     })
 }
+
+// ---------------- GET ----------------
 
 app.get("/", (req, res) => {
     res.status(200).send("Curso de Node.js")
@@ -33,10 +49,14 @@ app.get("/livros/:id", (req, res) => {
     res.status(200).json(livros[index])
 })
 
+// ---------------- POST ----------------
+
 app.post("/livros", (req, res) => {
     livros.push(req.body)
     res.status(201).send("Livro cadastrado com sucesso")
 })
+
+// ---------------- PUT ----------------
 
 app.put("/livros/:id", (req, res) => {
     const index = buscaLivro(req.params.id)
@@ -44,4 +64,14 @@ app.put("/livros/:id", (req, res) => {
     res.status(200).json(livros)
 })
 
+// ---------------- DELETE ----------------
+
+app.delete("/livros/:id", (req, res) => {
+    const index = buscaLivro(req.params.id)
+    livros.splice(index, 1)
+    res.status(200).send("Livro removido com sucesso")
+})
+
 export default app
+
+// 
