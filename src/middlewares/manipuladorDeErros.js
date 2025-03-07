08d906eaa5826c872203ffa87dcd86981f6f9574
Eduardo Kamo <eduardokamoz@gmail.com>
2025-03-07 15:10:19 -0300

    Refatoração
    Refatorado as tratativas de erros

--- src/middlewares/manipuladorDeErros.js
+++ src/middlewares/manipuladorDeErros.js
@@ -1,10 +1,15 @@
 import mongoose from "mongoose"
+import ErroBase from "../erros/ErroBase.js"
+import RequisicaoIncorreta from "../erros/RequisicaoIncorreta.js"
+import ErroValidacao from "../erros/ErroValidacao.js"
 
 function manipuladorDeErros(erro, req, res, next) {
     if (erro instanceof mongoose.Error.CastError) {
-        res.status(400).send({ message: "Um ou mais dados fornecidos estão incorretos" })
+        new RequisicaoIncorreta().enviarResposta(res)
+    } else if (erro instanceof mongoose.Error.ValidationError) {
+        new ErroValidacao(erro).enviarResposta(res)
     } else {
-        res.status(500).send({ message: "Erro interno de servidor" })
+        new ErroBase().enviarResposta(res)
     }
 }
 
--- src/models/Livro.js
+++ src/models/Livro.js
@@ -3,11 +3,17 @@ import { autorSchema } from "./Autor.js"
 
 const livroSchema = new mongoose.Schema({
     id: { type: mongoose.Schema.Types.ObjectId },
-    titulo: { type: String, required: true },
-    editora: { type: String },
+    titulo: {
+        type: String,
+        required: [true, "O título do livro é obrigatório"]
+    },
+    autor: autorSchema,
+    editora: {
+        type: String,
+        required: [true, "A editora é obrigatória"]
+    },
     preco: { type: Number },
-    paginas: { type: Number },
-    autor: autorSchema
+    numeroPaginas: { type: Number }
 }, { versionKey: false })
 
 const livro = mongoose.model("livros", livroSchema)
