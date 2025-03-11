c7df2e5e9035708efd00e480a6a5814606c72ad9
Eduardo Kamo <eduardokamoz@gmail.com>
2025-03-11 09:07:40 -0300

    Adicionado algumas validações

--- src/models/Livro.js
+++ src/models/Livro.js
@@ -10,10 +10,18 @@ const livroSchema = new mongoose.Schema({
     autor: autorSchema,
     editora: {
         type: String,
-        required: [true, "A editora é obrigatória"]
+        required: [true, "A editora é obrigatória"],
+        enum: {
+            values: ["Casa do código", "Alura"],
+            message: "A editora {VALUE} não é um valor permitido"
+        }
     },
     preco: { type: Number },
-    numeroPaginas: { type: Number }
+    numeroPaginas: {
+        type: Number,
+        min: [10, "O número de páginas de estar entre 10 e 5000. Valor fornecido: {VALUE}"],
+        max: [5000, "O número de páginas de estar entre 10 e 5000. Valor fornecido: {VALUE}"]
+    }
 }, { versionKey: false })
 
 const livro = mongoose.model("livros", livroSchema)
