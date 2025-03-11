44459d2c0838c11c24ab06c28f849edad599359c
Eduardo Kamo <eduardokamoz@gmail.com>
2025-03-11 15:36:40 -0300

    Filtro

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -77,11 +77,20 @@ class LivroController {
         }
     }
 
-    static async listarLivrosPorEditora(req, res, next) {
-        const editora = req.query.editora
+    static async listarLivrosPorFiltro(req, res, next) {
         try {
-            const livrosPorEditora = await livro.find({ editora: editora })
+
+            const { editora, titulo } = req.query.editora
+
+            const busca = {}
+
+            if (editora) busca.editora = editora
+            if (titulo) busca.titulo = { $regex: titulo, $options: "i" }
+
+            const livrosPorEditora = await livro.find(busca)
+
             res.status(200).json(livrosPorEditora)
+
         } catch (erro) {
             next(erro)
         }
